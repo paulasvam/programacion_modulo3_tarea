f7a0c61b9de0e4d84ba629730915e7d83eb84bda
paulasvam <anapaulacv2004@gmail.com>
2025-03-26 11:55:33 -0400

    Add files via upload

---
+++ ejercicio20.cs
@@ -0,0 +1,27 @@
+string nombreApellido;
+nombreApellido = "Ana Paula Carruyo Valbuena";
+Console.WriteLine(nombreApellido);
+
+int edad;
+edad = 21;
+Console.WriteLine(edad);
+
+char letraFavorita;
+letraFavorita = 'P';
+Console.WriteLine(letraFavorita);
+
+float numeroImparDividido;
+numeroImparDividido = 3.5f;
+Console.WriteLine(numeroImparDividido);
+
+double numeroConDecimal;
+numeroConDecimal = 1.767348;
+Console.WriteLine(numeroConDecimal);
+
+decimal numeroDividido;
+numeroDividido = 7.6575665m;
+Console.WriteLine(numeroDividido);
+
+bool verdadFalso;
+verdadFalso = true;
+Console.WriteLine(verdadFalso);
---
+++ ejercicio21.cs
@@ -0,0 +1,16 @@
+Console.Write("Hello, ");
+string nombre;
+nombre = "Bob";
+Console.Write(nombre);
+
+Console.Write("! you have ");
+int numero;
+numero = 3;
+Console.Write(numero);
+Console.Write(" message in your inbox. ");
+
+Console.Write("The temperature is ");
+float numeroDecimal;
+numeroDecimal = 34.4f;
+Console.Write(numeroDecimal);
+Console.Write(" celcius.");
